3dbbde13c0c80a8077f9e5cbaad4974bf8d130e4
Fidellis-Taz <secretforex98@gmail.com>
2020-12-10 01:35:59 +0200

    send a request to the server ,after signup

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 // middleware
 app.use(express.static("public"));
+app.use(cookieParser());
 app.use(
   express.json()
 ); /* this will take the user input data and pass it into js object and we can now access the data in  controller : req.body */
@@ -28,3 +30,19 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+/* Cookies */
+
+//set the cookie
+app.get("/set-cookies", (req, ress) => {
+  res.cookie("newUser", true, { maxAge: 1000 * 60 * 60 * 24, httpOnly });
+
+  res.send("you got the cookie");
+});
+
+//read the cookie
+app.get("/read-cookies", (req, ress) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+});
