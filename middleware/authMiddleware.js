4c02f658a1677e3116b3587a38c1cd2c4c0c83b7
Fidellis-Taz <secretforex98@gmail.com>
2020-12-11 07:06:23 +0200

    display user email on navbar if logged in : conditionla rendering

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 /* check if there is jwt on the request  */
 const jwt = require("jsonwebtoken");
-
+const User = require("../models/User");
 const requireAuth = (req, res, next) => {
   /* grap the token  */
   const token = req.cookies.jwt;
@@ -25,4 +25,34 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+/* check current user if they are logged in or not so that we can answer their requests */
+
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    /* verify the available token */
+    jwt.verify(
+      token,
+      "type is a secret text that will be saved",
+      async (err, decodedToken) => {
+        if (err) {
+          console.log(err.message);
+          res.locals.user = null;
+          next();
+        } else {
+          console.log(decodedToken);
+
+          let user = await User.findById(decodedToken.id);
+          /* pass the properties of the user into the view by: */
+          res.locals.user = user;
+          next();
+        }
+      }
+    );
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
